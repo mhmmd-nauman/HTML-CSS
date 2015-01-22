461db5e0d2397730f63cacde106e23a24e0f4758
Muhammad Nauman <mhmmd.nauman@gmail.com>
2015-01-22 10:38:54 -0800

    its my test work

---
+++ test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <style type="text/css">
+            h1 {
+                color:blue; font-size: 10px; text-align: center; margin-top: 300px;
+            }
+        </style>  
+    </head>
+    <body>
+        <h1>My First PROGRAMM IN css</h1> 
+        <h1 style="color:red;">My Second PROGRAMM IN css</h1>
+    </body>
+</html>
