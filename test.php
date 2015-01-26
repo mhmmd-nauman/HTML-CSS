25d5de53cd4226728ee3b396af6fdcea35e0b0ff
Nabeel01 <muhammadadeelshafique@gmail.com>
2015-01-25 21:23:41 -0800

    test work

--- test.php
+++ test.php
@@ -9,13 +9,14 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
         <style type="text/css">
-            h1 {
+            p {
                 color:blue; font-size: 10px; text-align: center; margin-top: 300px;
             }
         </style>  
     </head>
     <body>
-        <h1>My First PROGRAMM IN css</h1> 
+        <p>My First PROGRAMM IN css</p>
+        <p>My First PROGRAMM IN css</p> 
         <h1 style="color:red;">My Second PROGRAMM IN css</h1>
     </body>
 </html>
